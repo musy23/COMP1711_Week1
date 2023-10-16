57b96e5dbd6c08a92507e0fa3b4f194260f5a1d3
Mustafa Syed <146679440+musy23@users.noreply.github.com>
2023-10-16 00:25:56 +0000

    Attempted worksheet2

--- worksheet1.c
+++ worksheet1.c
@@ -1,4 +1,4 @@
-//Exercise 3 
+//Exercise 2 
 #include <stdio.h>
 
 int main()
@@ -6,9 +6,9 @@ int main()
     int number1, number2;
     printf("Enter two numbers" );
     scanf("%d %d", &number1, &number2);
-qq
-    int number3 = number1 + number2
-    printf("The sum is= %d\n", number3)
+
+    int number3 = number1 + number2;
+    printf("The sum is= %d\n", number3);
 
     return 0;
 
